10e40f3939072362064db006221e769ee642fecd
Roberto Gutierrez <rgutierrez555@umail.ucsb.edu>
2020-01-29 09:37:25 -0800

    Prepared countDownFuncs.cpp for real this time

--- countDownFuncs.cpp
+++ countDownFuncs.cpp
@@ -5,27 +5,33 @@
 using namespace std;
 
 Picker::Picker(){
-    int s = 1;
-    for(int i = 0; i < 20; i++){
-        smallOnes[i] = s;
-        s++;
-        if(s > 10) s = s % 10;
-    }
-    s = 25;
-    for(int i = 0; i < 4; i++){
-        largeOnes[i] = s;
-        s+=25;
-    }
+    for(int i = 0; i < 20; i++)
+        smallOnes[i] = 1;
+    for(int i = 0; i < 4; i++)
+        largeOnes[i] = 1;
+    // int s = 1;
+    // for(int i = 0; i < 20; i++){
+    //     smallOnes[i] = s;
+    //     s++;
+    //     if(s > 10) s = s % 10;
+    // }
+    // s = 25;
+    // for(int i = 0; i < 4; i++){
+    //     largeOnes[i] = s;
+    //     s+=25;
+    // }
 };
 int* Picker::pickNumbers(int small, int large){
-    srand(time(0));
-    for(int i = 0; i < large; i++){
-        int l = (rand() % (4 - 1 + 1)) + 1;
-        cout<<largeOnes[l-1]<<" "; 
-    }
-    for (int i = 0; i < small; i++){
-        int s = (rand() % (20 - 1 + 1)) + 1;
-        if(i == small - 1) cout<<smallOnes[s-1]<<endl;
-        else cout<<smallOnes[s-1]<<" ";
-    }
+    // srand(time(0));
+    // for(int i = 0; i < large; i++){
+    //     int l = (rand() % (4 - 1 + 1)) + 1;
+    //     cout<<largeOnes[l-1]<<" "; 
+    // }
+    // for (int i = 0; i < small; i++){
+    //     int s = (rand() % (20 - 1 + 1)) + 1;
+    //     if(i == small - 1) cout<<smallOnes[s-1]<<endl;
+    //     else cout<<smallOnes[s-1]<<" ";
+    // }
+    int stub[6] = new int{1,1,1,1,1,1};
+    return stub;
 };
